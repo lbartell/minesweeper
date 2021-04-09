577e46035fb300e9d1b9e575d111f3af68c7503b
mflood <hermannwest@gmail.com>
2021-04-09 03:56:55 -0600

    Create DEFAULT_BOARD_SIZE and DEFAULT_NUM_BOMBS

--- lib/config.py
+++ lib/config.py
@@ -1,11 +1,13 @@
 from dataclasses import dataclass
 
+DEFAULT_BOARD_SIZE = 10
+DEFAULT_NUM_BOMBS = 5
 
 @dataclass
 class Configuration:
 
-    board_size: int = 10
-    num_bombs: int = 5
+    board_size: int = DEFAULT_BOARD_SIZE
+    num_bombs: int = DEFAULT_NUM_BOMBS
     show_outer_numbers: bool = False
 
     row_spacer = "\n"
